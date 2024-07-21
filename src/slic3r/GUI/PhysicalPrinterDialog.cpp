756cc03ab4cb914611b5c00364a5a4d5e6532b48
Ocraftyone <Ocraftyone@users.noreply.github.com>
2024-07-21 21:04:15 +0000

    Revert "Fix issue caused by the fact that the backing widget of the `TextCtrl` is no longer `wxTextCtrl`"
    SuperSlicer still uses wxTextCtrl as the sole backing widget of text fields

--- src/slic3r/GUI/PhysicalPrinterDialog.cpp
+++ src/slic3r/GUI/PhysicalPrinterDialog.cpp
@@ -645,7 +645,7 @@ void PhysicalPrinterDialog::update(bool printer_change)
 
         // hide pre-configured address, in case user switched to a different host type
         if (Field* printhost_field = m_optgroup->get_field("print_host"); printhost_field) {
-            if (wxTextCtrl* temp = dynamic_cast<TextCtrl*>(printhost_field)->text_ctrl(); temp) {
+            if (auto * temp = dynamic_cast<wxTextCtrl*>(printhost_field->getWindow()); temp) {
                 const auto current_host = temp->GetValue();
                 if (current_host == "https://simplyprint.io" || current_host == "https://simplyprint.io/panel") {
                     temp->SetValue(wxString());
@@ -682,14 +682,14 @@ void PhysicalPrinterDialog::update(bool printer_change)
             // Set the host url
             if (Field* printhost_field = m_optgroup->get_field("print_host"); printhost_field) {
                 printhost_field->disable();
-                if (wxTextCtrl* temp = dynamic_cast<TextCtrl*>(printhost_field)->text_ctrl(); temp && temp->GetValue().IsEmpty()) {
+                if (auto* temp = dynamic_cast<wxTextCtrl*>(printhost_field->getWindow()); temp && temp->GetValue().IsEmpty()) {
                     temp->SetValue("https://simplyprint.io");
                 }
                 m_config->opt_string("print_host") = "https://simplyprint.io";
             }
             if (Field* printhost_webui_field = m_optgroup->get_field("print_host_webui"); printhost_webui_field) {
                 printhost_webui_field->disable();
-                if (wxTextCtrl* temp = dynamic_cast<TextCtrl*>(printhost_webui_field)->text_ctrl(); temp && temp->GetValue().IsEmpty()) {
+                if (auto* temp = dynamic_cast<wxTextCtrl*>(printhost_webui_field->getWindow()); temp && temp->GetValue().IsEmpty()) {
                     temp->SetValue("https://simplyprint.io/panel");
                 }
                 m_config->opt_string("print_host_webui") = "https://simplyprint.io/panel";
